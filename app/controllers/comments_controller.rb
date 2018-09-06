b3a893ad5b9dc30f06e6a1d3bae011ff23f04a32
Nate Cottle and Will Swanson <nate@>
2018-09-06 13:44:36 -0700

    finish adding user_ids to posts and comments on their creation, display what comments/posts a user had created on their user page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,8 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:comment)
+      user_id = params[:comment][:user_id].to_i
+      params.require(:comment).permit(:comment, :user_id)
+      new_params = {:comment => params[:comment][:comment], :user_id => user_id}
     end
   end
